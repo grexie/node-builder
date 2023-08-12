c3f7e5a3c7dbaf3b0466dcc453f754b9039911d0
Tim Behrsin <tim.behrsin@grexie.com>
2023-08-12 15:39:16 +0100

    done

---
+++ builders/v8.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+ROOTDIR=$(pwd)
+
+git config --global user.name "Node Builder"
+git config --global user.email "node@localhost"
+git config --global core.autocrlf false
+git config --global core.filemode false
+git config --global color.ui true
+
+mkdir -p ~/node
+
+git clone git clone --depth 1 --branch ${NODE_VERSION} https://github.com/nodejs/node ~/node/node
+cd ~/node/node
+
+./configure --fully-static --enable-static
+
+make -j8
+
+cd out/
+zip -j -r  ~/node/node.zip .
+
