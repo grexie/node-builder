ce8d92f4639f274ec49be553c3c8348e08da62d1
Tim Behrsin <tim.behrsin@grexie.com>
2023-08-12 16:29:11 +0100

    done

--- builders/platform/android.sh
+++ builders/platform/android.sh
@@ -1,4 +1,28 @@
 
 function platform_deps() {
-  echo -n
+  sudo apt-get update
+  
+  sudo apt-get install -y \
+      pkg-config \
+      git \
+      subversion \
+      curl \
+      wget \
+      build-essential \
+      python2 \
+      xz-utils \
+      zip \
+      ninja-build
+
+  if [ "${ARCH}" == "arm64" ]; then
+    sudo apt-get install -y crossbuild-essential-arm64
+  fi
+
+  if [ "${ARCH}" == "arm" ]; then
+    sudo apt-get install -y crossbuild-essential-armhf
+  fi
+
+  if [ "${ARCH}" == "x86" ]; then
+    sudo apt-get install -y crossbuild-essential-i386
+  fi
 }
