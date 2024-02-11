65d4c95641c275f53b71e18ee76097401bd59bcd
Tim Behrsin <tim.behrsin@grexie.com>
2024-02-11 10:44:30 +0000

    Update linux.sh

--- builders/platform/linux.sh
+++ builders/platform/linux.sh
@@ -14,10 +14,24 @@ function platform_deps() {
 
   if [ "${ARCH}" == "arm64" ]; then
     sudo apt-get install -y crossbuild-essential-arm64
+    export CC_target=aarch64-linux-gnu-gcc
+    export CXX_target=aarch64-linux-gnu-g++
+    export CC_host="gcc"
+    export CXX_host="g++"
+    export CC="${CC_target}"
+    export CXX="${CXX_target}"
+    export NODE_CONFIGURE_FLAGS="--with-arm-float-abi=hard --with-arm-fpu=neon"
   fi
 
   if [ "${ARCH}" == "arm" ]; then
     sudo apt-get install -y crossbuild-essential-armhf
+    export CC_target=arm-linux-gnueabihf-gcc
+    export CXX_target=arm-linux-gnueabihf-g++
+    export CC_host="gcc -m32"
+    export CXX_host="g++ -m32"
+    export CC="${CC_target}"
+    export CXX="${CXX_target}"
+    export NODE_CONFIGURE_FLAGS="--with-arm-float-abi=hard --with-arm-fpu=neon"
   fi
 
   if [ "${ARCH}" == "x86" ]; then
