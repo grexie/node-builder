5f5f796bdaadca33fe46e3afc40eff48b52fe54a
Tim Behrsin <tim.behrsin@grexie.com>
2024-02-11 10:24:30 +0000

    Update mac.sh

--- builders/platform/mac.sh
+++ builders/platform/mac.sh
@@ -3,8 +3,13 @@ function platform_deps() {
   brew install ccache
   export CC_host="ccache cc -arch $(uname -m)"
   export CXX_host="ccache c++ -arch $(uname -m)"
-  export CC_target="ccache cc -arch ${ARCH}"
-  export CXX_target="ccache c++ -arch ${ARCH}"
+  if [ "${ARCH}" == "x64" ]; then
+    export CC_target="ccache cc -arch x86_64"
+    export CXX_target="ccache c++ -arch x86_64"
+  else
+    export CC_target="ccache cc -arch ${ARCH}"
+    export CXX_target="ccache c++ -arch ${ARCH}"
+  fi
   export CC=${CC_target}
   export CXX=${CXX_target}
 }
