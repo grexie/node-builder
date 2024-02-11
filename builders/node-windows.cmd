25678672dc7fb06a47272f8b81ad54495d893c69
Tim Behrsin <tim.behrsin@grexie.com>
2024-02-11 12:03:35 +0000

    Update node-windows.cmd

--- builders/node-windows.cmd
+++ builders/node-windows.cmd
@@ -15,7 +15,7 @@ git clone --depth 1 --branch v%VERSION% https://github.com/nodejs/node
 cd node
 
 choco install boxstarter
-BOXSTARTERSHELL "Install-BoxstarterPackage https://raw.githubusercontent.com/nodejs/node/HEAD/tools/bootstrap/windows_boxstarter -DisableReboots"
+powershell "Import-Module Boxstarter.Chocolatey; Install-BoxstarterPackage https://raw.githubusercontent.com/nodejs/node/HEAD/tools/bootstrap/windows_boxstarter -DisableReboots"
 vcbuild dll
 
 cd out/Release
