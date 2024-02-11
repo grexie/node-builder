227d85aaf2203a6ae29f2996585dbb8134c4036b
Tim Behrsin <tim.behrsin@grexie.com>
2024-02-11 10:18:00 +0000

    Update mac.sh

--- builders/platform/mac.sh
+++ builders/platform/mac.sh
@@ -1,4 +1,9 @@
 
 function platform_deps() {
-  echo -n
+  export CC_host="ccache cc -arch $(uname -m)"
+  export CXX_host="ccache c++ -arch $(uname -m)"
+  export CC_target="ccache cc -arch ${ARCH}"
+  export CXX_target="ccache c++ -arch ${ARCH}"
+  export CC=${CC_target}
+  export CXX=${CXX_target}
 }
