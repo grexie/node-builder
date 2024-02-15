4c6e1f970e6f12390808276751cf6162f7faf0ab
Tim Behrsin <tim.behrsin@grexie.com>
2024-02-15 22:21:34 +0000

    Update node-windows.cmd

--- builders/node-windows.cmd
+++ builders/node-windows.cmd
@@ -20,5 +20,5 @@ powershell "Import-Module Boxstarter.Chocolatey; Install-BoxstarterPackage https
 vcbuild dll
 
 cd out/Release
-del .deps obj.host obj.target obj
-call 7z a %HOMEPATH%\node\node.zip -r *
+del /s /q .deps obj.host obj.target obj
+call 7z a %HOMEPATH%\node.zip -r *
