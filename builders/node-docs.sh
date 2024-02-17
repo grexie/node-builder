54787f2731ec1455f2972a911a86af2c3ae64b12
Tim Behrsin <tim.behrsin@grexie.com>
2024-02-17 01:58:42 +0000

    Update node-docs.sh

--- builders/node-docs.sh
+++ builders/node-docs.sh
@@ -4,9 +4,6 @@ set -e
 
 ROOTDIR=$(pwd)
 
-. ${ROOTDIR}/builders/arch/${ARCH}.sh
-. ${ROOTDIR}/builders/platform/${PLATFORM}.sh
-
 git config --global user.name "Node Builder"
 git config --global user.email "node@localhost"
 git config --global core.autocrlf false
@@ -18,7 +15,5 @@ mkdir -p ~/node
 cd ~/node
 git clone --depth 1 --branch v${VERSION} https://github.com/nodejs/node
 cd ~/node/node
-
-./configure --shared --dest-cpu ${ARCH} --dest-os ${PLATFORM} --cross-compiling --without-npm ${NODE_CONFIGURE_FLAGS}
 
 make doc
