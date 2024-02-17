c8516515c541a78faa3484d96b34cba1d7529a4d
Tim Behrsin <tim.behrsin@grexie.com>
2024-02-17 01:52:34 +0000

    Create node-docs.sh

---
+++ builders/node-docs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+set -e
+
+ROOTDIR=$(pwd)
+
+. ${ROOTDIR}/builders/arch/${ARCH}.sh
+. ${ROOTDIR}/builders/platform/${PLATFORM}.sh
+
+git config --global user.name "Node Builder"
+git config --global user.email "node@localhost"
+git config --global core.autocrlf false
+git config --global core.filemode false
+git config --global color.ui true
+
+mkdir -p ~/node
+
+cd ~/node
+git clone --depth 1 --branch v${VERSION} https://github.com/nodejs/node
+cd ~/node/node
+
+
+make doc
