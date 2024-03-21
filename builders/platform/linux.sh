3e2beb4b60aaea2cc3839533e018b4920563f0ee
Tim Behrsin <tim.behrsin@grexie.com>
2024-03-21 22:13:43 +0000

    Update linux.sh

--- builders/platform/linux.sh
+++ builders/platform/linux.sh
@@ -14,23 +14,35 @@ function platform_deps() {
 
   if [ "${ARCH}" == "arm64" ]; then
     sudo apt-get install -y crossbuild-essential-arm64
+    export AR_target=aarch64-linux-gnu-ar
     export CC_target=aarch64-linux-gnu-gcc
     export CXX_target=aarch64-linux-gnu-g++
+    export LINK_target=aarch64-linux-gnu-g++
+    export AR_host="ar"
     export CC_host="gcc"
     export CXX_host="g++"
+    export LINK_host="g++"
+    export AR="${AR_target}"
     export CC="${CC_target}"
     export CXX="${CXX_target}"
+    export LINK="${LINK_target}"
     export NODE_CONFIGURE_FLAGS="--with-arm-float-abi=hard --with-arm-fpu=neon"
   fi
 
   if [ "${ARCH}" == "arm" ]; then
     sudo apt-get install -y crossbuild-essential-armhf
+    export AR_target=arm-linux-gnueabihf-ar
     export CC_target=arm-linux-gnueabihf-gcc
     export CXX_target=arm-linux-gnueabihf-g++
+    export LINK_target=arm-linux-gnueabihf-g++
+    export AR_host="ar -m32"
     export CC_host="gcc -m32"
     export CXX_host="g++ -m32"
+    export LINK_host="g++ -m32"
+    export AR="${AR_target}"
     export CC="${CC_target}"
     export CXX="${CXX_target}"
+    export LINK="${LINK_target}"
     export NODE_CONFIGURE_FLAGS="--with-arm-float-abi=hard --with-arm-fpu=neon"
   fi
 
